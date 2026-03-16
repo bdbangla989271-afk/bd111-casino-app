654f3eb849bd4ac95e7c2158f6a78eaff2cad139
bdbangla989271-afk <bdbangla989271@gmail.com>
2026-03-16 06:07:32 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const symbols = ["🍒","🍋","🍊","🍉","🍇","7️⃣"];
 
+function spin(){
+  const s1 = symbols[Math.floor(Math.random()*symbols.length)];
+  const s2 = symbols[Math.floor(Math.random()*symbols.length)];
+  const s3 = symbols[Math.floor(Math.random()*symbols.length)];
+  
+  document.getElementById("slot1").innerText = s1;
+  document.getElementById("slot2").innerText = s2;
+  document.getElementById("slot3").innerText = s3;
+  
+  if(s1===s2 && s2===s3){
+    document.getElementById("result").innerText = "🎉 You Won! +10 Coins";
+  } else {
+    document.getElementById("result").innerText = "Try Again!";
+  }
+}
